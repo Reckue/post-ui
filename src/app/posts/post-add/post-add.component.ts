8425def303183896fef51ea216676902d8ef796f
Sherzod Mamadaliev <egnaf@yahoo.com>
2020-06-14 12:13:46 +0300

    Fix project structure and design #23

---
+++ src/app/posts/post-add/post-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-add',
+  templateUrl: './post-add.component.html',
+  styleUrls: ['./post-add.component.css']
+})
+export class PostAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/models/Post.ts
@@ -0,0 +1,9 @@
+export class Post {
+  id: string;
+  title: string;
+
+  constructor(id: string, title: string) {
+    this.id = id;
+    this.title = title;
+  }
+}
