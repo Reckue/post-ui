4aec2b7843ce01211a3689b971232ebc6826035f
Hardelele <Hardelele@yahoo.com>
2020-06-28 11:16:52 +0300

    Complete constructor

--- src/app/models/Post.ts
+++ src/app/models/Post.ts
@@ -2,6 +2,7 @@ import {Tag} from './Tag';
 import {Node} from './Node';
 import {StatusType} from './StatusType';
 
+
 export class Post {
 
   id: string;
@@ -14,7 +15,15 @@ export class Post {
   changed: number;
   status: StatusType;
 
-  constructor(id: string, title: string) {
+  constructor(id: string = null,
+              title: string = null,
+              nodes: Node[] = [],
+              source: string = '',
+              userId: string = '',
+              tags: Tag[] = [],
+              published: number = Date.now(),
+              changed: number = Date.now(),
+              status: StatusType = StatusType.ACTIVE) {
     this.id = id;
     this.title = title;
   }
