69dd89e303386d3d887761bb770484a375d8b6ee
Hardelele <Hardelele@yahoo.com>
2020-06-28 11:10:09 +0300

    Complete Post model

--- src/app/models/Post.ts
+++ src/app/models/Post.ts
@@ -1,6 +1,18 @@
+import {Tag} from './Tag';
+import {Node} from './Node';
+import {StatusType} from './StatusType';
+
 export class Post {
+
   id: string;
   title: string;
+  nodes: Node[];
+  source: string;
+  userId: string;
+  tags: Tag[];
+  published: number;
+  changed: number;
+  status: StatusType;
 
   constructor(id: string, title: string) {
     this.id = id;
