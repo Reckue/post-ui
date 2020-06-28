2b1b430945da547cc29980183fd2273eaf1258be
BIG-DAN-SO-BIG <DohovDO@mail.ru>
2020-06-28 21:46:09 +0300

    Create VideoNode model

---
+++ src/app/models/VideoNode.ts
@@ -0,0 +1,11 @@
+import {NodeContent} from './NodeContent';
+
+export class VideoNode implements NodeContent {
+
+  videoUrl: string;
+
+  constructor(videoUrl: string = null) {
+    this.videoUrl = videoUrl;
+  }
+
+}
