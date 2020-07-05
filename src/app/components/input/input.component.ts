1a0002e333470d053260aa46d769f6d569b17040
Hardelele <Hardelele@yahoo.com>
2020-07-05 04:54:51 +0300

    Add input component

---
+++ src/app/components/input/input.component.ts
@@ -0,0 +1,34 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent implements OnInit {
+
+  input: HTMLTextAreaElement;
+
+  @Input() id: string;
+  @Input() placeholder: string;
+  @Input() change: (event: any) => void;
+  @Input() value = null;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.input = window.document.getElementById(this.id) as HTMLTextAreaElement;
+    this.input.value = '';
+    this.input.cols = 0;
+    this.resize();
+  }
+
+  resize() {
+    this.input.style.height = (this.input.cols * 20) + 'px';
+    console.log('\'' + this.input.value + '\'');
+  }
+
+  onChange(event: Event) {
+    this.resize();
+    this.change(event);
+  }
+}
