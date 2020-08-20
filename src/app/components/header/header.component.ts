ebe6381563ea151d0fa3ecbd43cd24def788601d
Hardelele <Hardelele@yahoo.com>
2020-08-20 13:39:47 +0300

    Add header tracking

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, HostListener, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.pug',
-  styleUrls: ['./header.component.sass']
+  styleUrls: ['./header.component.sass'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  @ViewChild('header') header;
+  private headerPosition = 0;
 
   constructor() { }
 
-  ngOnInit(): void {
+  @HostListener('window:scroll')
+  track() {
+    this.calculatePosition(window.pageYOffset);
+    const trackStyle = 'top:-' + (window.pageYOffset - this.headerPosition) + 'px';
+    this.header.nativeElement.setAttribute('style', trackStyle);
   }
 
+  calculatePosition = (currentPosition) => {
+    if (currentPosition - this.headerPosition >= 60) {
+      this.headerPosition = currentPosition - 60;
+    } else if (currentPosition - this.headerPosition < 0) {
+      this.headerPosition = currentPosition;
+    }
+  }
 }
